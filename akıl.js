99b894cd00fc1c2fbc1e4b5a031f32d72a39bdbc
Mesut Buğra UYSAL <121828825+bugra123uysal@users.noreply.github.com>
2024-05-07 02:44:15 +0300

    Update akıl.js

--- akıl.js
+++ akıl.js
@@ -1,31 +1,84 @@
 
-/* sayılar */
+/* sayılar
+
+
 var sy=Math.floor(Math.random() *10)+1
 
-document.getElementById("ssa").innerHTML=sy
+    document.getElementById("ssa").innerHTML=sy
+
+
+
+    
+*/
+
+
+
+
+    
 
 
 /* harf */
 
+function bşl(){
+
 const alphabet = 'abcçdefghiıjklmnoçprstuüvyz';
+var a=alphabet[(Math.floor(Math.random()*alphabet.length))]
+var b=alphabet[(Math.floor(Math.random()*alphabet.length))]
+var c=alphabet[(Math.floor(Math.random()*alphabet.length))]
+var d=alphabet[(Math.floor(Math.random()*alphabet.length))]
+var e=alphabet[(Math.floor(Math.random()*alphabet.length))]
+var f=alphabet[(Math.floor(Math.random()*alphabet.length))]
 
- var a=alphabet[(Math.floor(Math.random()*alphabet.length))]
- var b=alphabet[(Math.floor(Math.random()*alphabet.length))]
- var c=alphabet[(Math.floor(Math.random()*alphabet.length))]
- var d=alphabet[(Math.floor(Math.random()*alphabet.length))]
+    document.getElementById("a").innerHTML=a
+    document.getElementById("b").innerHTML=b
+    document.getElementById("c").innerHTML=c
+    document.getElementById("d").innerHTML=d
+    document.getElementById("e").innerHTML=e
+    document.getElementById("f").innerHTML=f
 
- var e=alphabet[(Math.floor(Math.random()*alphabet.length))]
+    var  aa= document.getElementById("aa").style.display="none"
+    var  bb= document.getElementById("bb").style.display="none"
+    var  cc= document.getElementById("cc").style.display="none"
+    var  dd= document.getElementById("dd").style.display="none"
+    var  ee= document.getElementById("ee").style.display="none"
+    var  ff= document.getElementById("ff").style.display="none"
 
- var f=alphabet[(Math.floor(Math.random()*alphabet.length))]
- 
+    
+    setTimeout (function(){
+        document.getElementById("a").style.display="none"
+        document.getElementById("b").style.display="none"
+        document.getElementById("c").style.display="none"
+        document.getElementById("d").style.display="none"
+        document.getElementById("e").style.display="none"
+        document.getElementById("f").style.display="none"
+    
+        var  aa= document.getElementById("aa").style.display="block"
+        var  bb= document.getElementById("bb").style.display="block"
+        var  cc= document.getElementById("cc").style.display="block"
+        var  dd= document.getElementById("dd").style.display="block"
+        var  ee= document.getElementById("ee").style.display="block"
+        var  ff= document.getElementById("ff").style.display="block"
+               
+            },10000);
+            
 
-document.getElementById("a").innerHTML=a
-document.getElementById("b").innerHTML=b
-document.getElementById("c").innerHTML=c
-document.getElementById("d").innerHTML=d
-document.getElementById("e").innerHTML=e
-document.getElementById("f").innerHTML=f
+    /* cevap */
+
+   var  aa= document.getElementById("aa").value;
+   var  bb= document.getElementById("bb").value;
+   var  cc= document.getElementById("cc").value;
+   var  dd= document.getElementById("dd").value;
+   var  ee= document.getElementById("ee").value;
+   var  ff= document.getElementById("ff").value;
+function cvpp(){
+  
 
 
 
 
+
+
+}
+
+
+}
