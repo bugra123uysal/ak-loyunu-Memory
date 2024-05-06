effb27ef4e44667cbe7d83509fce5847a9282254
Mesut Buğra UYSAL <121828825+bugra123uysal@users.noreply.github.com>
2024-05-06 03:20:47 +0300

    Create akıl.js

---
+++ akıl.js
@@ -0,0 +1,31 @@
+
+/* sayılar */
+var sy=Math.floor(Math.random() *10)+1
+
+document.getElementById("ssa").innerHTML=sy
+
+
+/* harf */
+
+const alphabet = 'abcçdefghiıjklmnoçprstuüvyz';
+
+ var a=alphabet[(Math.floor(Math.random()*alphabet.length))]
+ var b=alphabet[(Math.floor(Math.random()*alphabet.length))]
+ var c=alphabet[(Math.floor(Math.random()*alphabet.length))]
+ var d=alphabet[(Math.floor(Math.random()*alphabet.length))]
+
+ var e=alphabet[(Math.floor(Math.random()*alphabet.length))]
+
+ var f=alphabet[(Math.floor(Math.random()*alphabet.length))]
+ 
+
+document.getElementById("a").innerHTML=a
+document.getElementById("b").innerHTML=b
+document.getElementById("c").innerHTML=c
+document.getElementById("d").innerHTML=d
+document.getElementById("e").innerHTML=e
+document.getElementById("f").innerHTML=f
+
+
+
+
