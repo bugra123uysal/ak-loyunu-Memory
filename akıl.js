74f1b6941f2cfed20e55583b3d16500823a18ce2
Mesut Buğra UYSAL <121828825+bugra123uysal@users.noreply.github.com>
2024-05-11 02:22:07 +0300

    Update akıl.js

--- akıl.js
+++ akıl.js
@@ -1,5 +1,10 @@
 
+
+
 /* sayılar */
+ 
+
+
 
 /* başla button */
 function alşab(){
@@ -12,8 +17,6 @@ function alşab(){
     document.getElementById("dort").innerHTML=Math.floor(Math.random()*10*1);
     document.getElementById("bes").innerHTML=Math.floor(Math.random()*10*1);
     document.getElementById("altı").innerHTML=Math.floor(Math.random()*10*1);
-
-
 
 
     var bırr=document.getElementById("bırr").style.display="none"
@@ -29,26 +32,28 @@ var uc=document.getElementById("uc").value;
 var dort=document.getElementById("dort").value;
 var bes=document.getElementById("bes").value;
 var altı=document.getElementById("altı").value;    
+/*  
+ /* saniye belirleme 
+ var süree=document.getElementById("frst").value;
+ */
 
 setTimeout (function () {
+   
     var bır=document.getElementById("bır").style.display="none"
-var ıkı=document.getElementById("ıkı").style.display="none"
-var uc=document.getElementById("uc").style.display="none"
-var dort=document.getElementById("dort").style.display="none"
-var bes=document.getElementById("bes").style.display="none"
-var altı=document.getElementById("altı").style.display="none"
-
-var bırr=document.getElementById("bırr").style.display="block"
-var ıkıı=document.getElementById("ıkıı").style.display="block"
-var ucc=document.getElementById("ucc").style.display="block"
-var dortt=document.getElementById("dortt").style.display="block"
-var bess=document.getElementById("bess").style.display="block"
-var altıı=document.getElementById("altıı").style.display="block"
-
-
-},6000);
+    var ıkı=document.getElementById("ıkı").style.display="none"
+    var uc=document.getElementById("uc").style.display="none"
+    var dort=document.getElementById("dort").style.display="none"
+    var bes=document.getElementById("bes").style.display="none"
+    var altı=document.getElementById("altı").style.display="none"
     
-
+    var bırr=document.getElementById("bırr").style.display="block"
+    var ıkıı=document.getElementById("ıkıı").style.display="block"
+    var ucc=document.getElementById("ucc").style.display="block"
+    var dortt=document.getElementById("dortt").style.display="block"
+    var bess=document.getElementById("bess").style.display="block"
+    var altıı=document.getElementById("altıı").style.display="block"
+},4000);
+ 
 }
 
 
@@ -89,20 +94,14 @@ if(bır===bırr && ıkı===ıkıı &&uc===ucc&& dort===dortt && bes===bess  && a
 
 
 
-
-
-
-
-
-
-
-
-
-
 var oo=document.getElementById("ğx").value;
 
 
 var mm=document.getElementById("ğx").innerHTML=frst
+
+
+/
+
 
 
 
@@ -131,8 +130,12 @@ var f=alphabet[(Math.floor(Math.random()*alphabet.length))]
     var  dd= document.getElementById("dd").style.display="none"
     var  ee= document.getElementById("ee").style.display="none"
     var  ff= document.getElementById("ff").style.display="none"
+ /* saniye belirleme */
+ var süree=document.getElementById("frst").value;
 
-    
+    /* saniye belirleme 
+ var süree=document.getElementById("frst").value;
+ */
     setTimeout (function(){
         document.getElementById("a").style.display="none"
         document.getElementById("b").style.display="none"
@@ -148,7 +151,7 @@ var f=alphabet[(Math.floor(Math.random()*alphabet.length))]
         var  ee= document.getElementById("ee").style.display="block"
         var  ff= document.getElementById("ff").style.display="block"
                
-            },5000);
+            }, 4000);
         }      
 
     /* cevap */
@@ -184,4 +187,88 @@ var f=alphabet[(Math.floor(Math.random()*alphabet.length))]
 
 
    }
+
+/* senbol */
+/* başla */
+function la(){
+    const snbll="!^+%&/)(=?_|}][½$#£/*-+"
+    var sa=snbll[Math.floor(Math.random()*snbll.length) ]
+    var sb=snbll[Math.floor(Math.random()*snbll.length) ]
+    var sc=snbll[Math.floor(Math.random()*snbll.length) ]
+    var sd=snbll[Math.floor(Math.random()*snbll.length) ]
+    var se=snbll[Math.floor(Math.random()*snbll.length) ]
+    var sf=snbll[Math.floor(Math.random()*snbll.length) ]
+
+/* cevaplar gizli */
+var z=document.getElementById("z").style.display="none"
+var x=document.getElementById("x").style.display="none" 
+var v=document.getElementById("v").style.display="none" 
+var n=document.getElementById("n").style.display="none" 
+var m=document.getElementById("m").style.display="none" 
+var ö=document.getElementById("ö").style.display="none" 
+
+/* senboller */
+    document.getElementById("sa").innerHTML=sa
+    document.getElementById("sb").innerHTML=sb
+    document.getElementById("sc").innerHTML=sc
+    document.getElementById("sd").innerHTML=sd
+    document.getElementById("se").innerHTML=se
+    document.getElementById("sf").innerHTML=sf
+
+    setTimeout (function () {
+
+        /* cevaplar */
+
+var z=document.getElementById("z").style.display="block"
+var x=document.getElementById("x").style.display="block" 
+var v=document.getElementById("v").style.display="block" 
+var n=document.getElementById("n").style.display="block" 
+var m=document.getElementById("m").style.display="block" 
+var ö=document.getElementById("ö").style.display="block" 
+
+/* senbol */
+var z=document.getElementById("sa").style.display="none"
+var x=document.getElementById("sb").style.display="none" 
+var v=document.getElementById("sc").style.display="none" 
+var n=document.getElementById("sd").style.display="none" 
+var m=document.getElementById("se").style.display="none" 
+var ö=document.getElementById("sf").style.display="none" 
+        
+    
+
+    },4000)
+
+
+
+
+}
+/* bittir */
+function ir(){
+/* gelen senboller */
+var as= document.getElementById("sa").value
+var bs=  document.getElementById("sb").value
+var  cs= document.getElementById("sc").value
+var  ds= document.getElementById("sd").value
+var  es= document.getElementById("se").value
+var  fs= document.getElementById("sf").value
+
+
+    /* cevaplar */
+var z=document.getElementById("z").value 
+var x=document.getElementById("x").value 
+var v=document.getElementById("v").value 
+var n=document.getElementById("n").value 
+var m=document.getElementById("m").value 
+var ö=document.getElementById("ö").value 
+
+if( as===z && bs===x&& v===cs && n===ds&& es===m && fs===ö ){
+alert("tebrikler")
+
+}else{
+    alert("tekrar deneyiniz")
+}
+
+
+}
+
 
